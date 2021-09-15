a724d3ee49ad3d62f984913ee58d71b79986a24c
IPerovv <perov.kaa@gmail.com>
2021-09-15 14:39:11 +0300

    first lab

---
+++ lab1/Perov_Ivan_lab1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+
+	int voroni;
+	cout << "skolko voron? \n";
+	cin >> voroni;
+
+
+	if (voroni % 10 == 1 and voroni != 11) {
+		cout << "V stae " << voroni << " Vorona";
+	}
+	else if ((voroni % 10 == 2 or voroni % 10 == 3 or voroni % 10 == 4) and voroni != 12 and voroni != 13 and voroni != 14) {
+		cout << "V stae " << voroni << " Voroni";
+	}
+	else {
+		cout << "V stae " << voroni << " Voron";
+	}
+	return 0;
+}
