08a454b8235487413972439bb9048ddb82ce6118
IPerovv <perov.kaa@gmail.com>
2021-09-15 15:18:59 +0300

    Repaired Lab

--- lab1/Perov_Ivan_lab1.cpp
+++ lab1/Perov_Ivan_lab1.cpp
@@ -17,23 +17,5 @@ int main() {
 	else {
 		cout << "V stae " << voroni << " Voron";
 	}	return 0;
-}
 
-int pain() {
-
-
-	int voroni;
-	cout << "skolko voron? \n";
-	cin >> voroni;
-
-
-	if (voroni % 10 == 1 and voroni != 11) {
-		cout << "V stae " << voroni << " Vorona";
-	}
-	else if ((voroni % 10 == 2 or voroni % 10 == 3 or voroni % 10 == 4) and voroni != 12 and voroni != 13 and voroni != 14) {
-		cout << "V stae " << voroni << " Voroni";
-	}
-	else {
-		cout << "V stae " << voroni << " Voron";
-	}	return 0;
 }
