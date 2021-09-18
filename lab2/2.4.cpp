eba98cd173d8e6227139d5e17ca691ad513165ab
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-19 00:04:55 +0300

    2.4

--- lab2/2.4.cpp
+++ lab2/2.4.cpp
@@ -1,4 +1,17 @@
-//
-// Created by 01 on 18.09.2021.
-//
+#include <iostream>
 
+int main()
+{
+    int a, b, c;
+    std::cout << "Enter three numbers" << std::endl;
+    std::cin >> a >> b >> c;
+    if ((a % 2 == 0 || b % 2 == 0 || c % 2 == 0) && ((a % 2 != 0 || b % 2 != 0 || c % 2 != 0)))
+    {
+        std::cout << "Yes";
+    }
+    else
+    {
+        std::cout << "No";
+    }
+    return 0;
+}
