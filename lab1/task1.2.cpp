b346a41e2a787b683284397ce3a15da8dcbb6146
ce7er2s <spike.calhun@yandex.ru>
2021-09-18 15:45:06 +0300

    task 1.2.cpp improvement

--- lab1/task1.2.cpp
+++ lab1/task1.2.cpp
@@ -4,7 +4,7 @@ int get_int() {
     int temp = 0;
     std::cin >> temp;
     if (std::cin.fail() or std::cin.peek() != '\n') {
-        std::cout << "Коэфициент должен быть целочисленным\n";
+        std::cout << "Коэфициент должен быть целочисленным.\n";
         std::exit(1);
     }
     return temp;
@@ -14,7 +14,11 @@ int main() {
     std::cout << "Введите количество ворон." << std::endl;
     int ravensCount = 0;
     ravensCount = get_int();
-    if ((ravensCount % 100 > 10) && (ravensCount < 20)) {
+	if (ravensCount < 0) {
+		std::cout << "Количество ворон не можеть быть меньше нуля." << std::endl;
+		return -1;
+	}
+    if (((ravensCount % 100 > 10) && (ravensCount < 20)) || (ravensCount == 0)) {
         std::cout << "В стае " << ravensCount << " ворон." << std::endl;
     } else if (ravensCount % 10 == 1) {
         std::cout << "В стае " << ravensCount << " ворона." << std::endl;
