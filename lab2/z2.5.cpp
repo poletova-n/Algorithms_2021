9751b130299ced8f9d9e9d91f77e091dc538225e
S-4I5 <yasha.poluektovv@gmail.com>
2021-09-20 02:22:32 +0300

    Lab2 z2.5 code fix

--- lab2/z2.5.cpp
+++ lab2/z2.5.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
-void compCoords(int* pos1, int* pos2);
-
-int* rC(int* arr);
-
 int main()
 {
     int pos1[2] = {0, 0};
@@ -24,35 +20,14 @@ int main()
         }
         else
         {
-            compCoords(rC(pos1), rC(pos2));
+            if(pos1[0]* pos2[0] > 0 && pos1[1] * pos2[1] > 0)
+            {
+                std::cout << "YES";
+            }
+            else
+            {
+                std::cout << "NO";
+            }
         }
     }
-}
-
-void compCoords(int* pos1, int* pos2)
-{
-    if(pos1[0] == pos2[0] && pos1[1] == pos2[1])
-    {
-        std::cout << "YES";
-    }
-    else
-    {
-        std::cout << "NO";
-    }
-}
-
-int* rC(int* arr)
-{
-    for (int i = 0; i < 2; ++i)
-    {
-        if(arr[i] > 0)
-        {
-            arr[i] = 1;
-        }
-        else
-        {
-            arr[i] = 0;
-        }
-    }
-    return arr;
 }
