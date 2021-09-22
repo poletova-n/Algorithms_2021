6cbd168aeb0fb8b6c7877807c846f186186a8dec
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 15:06:26 +0300

    2.5

--- lab2/1.2.cpp
+++ lab2/1.2.cpp
@@ -19,7 +19,7 @@ int main()
         }
         else if (l1 < 1 or l2 < 1 or col1 < 1 or col2 < 1 or l1 > 8 or l2 > 8 or col1 > 8 or col2 > 8)
         {
-            std::cout << "The coordinates of the figures must be greatest than one less than 8. End of the program";
+            std::cout << "The coordinates of the figures must be greatest than zero less than nine. End of the program";
         }
         else
         {
