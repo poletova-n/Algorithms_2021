b3f71170ae700c1cf175552dfbeaf863851f1901
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:24:34 +0300

    2.5

--- lab2/2.2.cpp
+++ lab2/2.2.cpp
@@ -5,47 +5,54 @@ int main()
     int a, b, c, x1, x2, x3;
     std::cout << "Enter three numbers" << std::endl;
     std::cin >> a >> b >> c;
-    if (a < b && a < c)
+    if (std::cin.peek() != '\n')
     {
-        x1 = a;
-        if (b < c)
-        {
-            x2 = b;
-            x3 = c;
-        }
-        else
-        {
-            x2 = c;
-            x3 = b;
-        }
+        std::cout << "Numbers must be entered. End of the program.";
     }
-    if (b < a && b < c)
+    else
     {
-        x1 = b;
-        if (a < c)
+        if (a < b && a < c)
         {
-            x2 = a;
-            x3 = c;
+            x1 = a;
+            if (b < c)
+            {
+                x2 = b;
+                x3 = c;
+            }
+            else
+            {
+                x2 = c;
+                x3 = b;
+            }
         }
-        else
+        if (b < a && b < c)
         {
-            x2 = c;
-            x3 = a;
+            x1 = b;
+            if (a < c)
+            {
+                x2 = a;
+                x3 = c;
+            }
+            else
+            {
+                x2 = c;
+                x3 = a;
+            }
         }
+        if (c < b && c < a)
+        {
+            x1 = c;
+            if (b < a)
+            {
+                x2 = b;
+                x3 = a;
+            }
+            else
+            {
+                x2 = a;
+                x3 = b;
+            }
+        }
+        std::cout << x1 << " " << x2 << " " << x3;
     }
-    if (c < b && c < a)
-    {
-        x1 = c;
-        if (b < a)
-        {
-            x2 = b;
-            x3 = a;
-        }
-        else
-        {
-            x2 = a;
-            x3 = b;
-        }
-    }
-    std::cout << x1 << " " << x2 << " " << x3;
 }
