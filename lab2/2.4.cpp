dfd4c1c37ce0a9962a2da04e17db370c281167fb
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:26:00 +0300

    2.5

--- lab2/2.4.cpp
+++ lab2/2.4.cpp
@@ -5,13 +5,20 @@ int main()
     int a, b, c;
     std::cout << "Enter three numbers" << std::endl;
     std::cin >> a >> b >> c;
-    if ((a % 2 == 0 || b % 2 == 0 || c % 2 == 0) && ((a % 2 != 0 || b % 2 != 0 || c % 2 != 0)))
+    if (std::cin.peek() != '\n')
     {
-        std::cout << "Yes";
+        std::cout << "Numbers must be entered. End of the program.";
     }
     else
     {
-        std::cout << "No";
+        if ((a % 2 == 0 || b % 2 == 0 || c % 2 == 0) && ((a % 2 != 0 || b % 2 != 0 || c % 2 != 0)))
+        {
+            std::cout << "Yes";
+        }
+        else
+        {
+            std::cout << "No";
+        }
+        return 0;
     }
-    return 0;
 }
