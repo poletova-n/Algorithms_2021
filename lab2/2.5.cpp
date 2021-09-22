d80b93088f774bb0895a207629f3146294634743
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:28:07 +0300

    2.5

--- lab2/2.5.cpp
+++ lab2/2.5.cpp
@@ -5,14 +5,28 @@ int main()
     int x1, y1, x2, y2;
     std::cout << "Enter the coordinates of the first point:" << std::endl;
     std::cin >> x1 >> y1;
-    std::cout << "Enter the coordinates of the second point:" << std::endl;
-    std::cin >> x2 >> y2;
-    if (((x1 * x2) > 0) && ((y1 * y2) > 0))
+    if (std::cin.peek() != '\n')
     {
-        std::cout << "Yes";
+        std::cout << "The coordinates of the first point must be numbers. End of the program.";
     }
     else
     {
-        std::cout << "No";
+        std::cout << "Enter the coordinates of the second point:" << std::endl;
+        std::cin >> x2 >> y2;
+        if (std::cin.peek() != '\n')
+        {
+            std::cout << "The coordinates of the second point must be numbers. End of the program.";
+        }
+        else
+        {
+            if (((x1 * x2) > 0) && ((y1 * y2) > 0))
+            {
+                std::cout << "Yes";
+            }
+            else
+            {
+                std::cout << "No";
+            }
+        }
     }
 }
