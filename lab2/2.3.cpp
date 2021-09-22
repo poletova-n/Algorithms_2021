18871f28ce63798db9aba305534b8ed2af374b03
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:25:12 +0300

    2.5

--- lab2/2.3.cpp
+++ lab2/2.3.cpp
@@ -5,16 +5,23 @@ int main()
     int a, b, c;
     std::cout << "Enter three numbers" << std:: endl;
     std::cin >> a >> b >> c;
-    if ((a == b) && (a == c))
+    if (std::cin.peek() != '\n')
     {
-        std::cout << "3 identical numbers";
-    }
-    else if ((a != c) && (a != b) && (b != c))
-    {
-        std::cout << "0 identical numbers";
+        std::cout << "Numbers must be entered. End of the program.";
     }
     else
     {
-        std::cout << "2 identical numbers";
+        if ((a == b) && (a == c))
+        {
+            std::cout << "3 identical numbers";
+        }
+        else if ((a != c) && (a != b) && (b != c))
+        {
+            std::cout << "0 identical numbers";
+        }
+        else
+        {
+            std::cout << "2 identical numbers";
+        }
     }
 }
