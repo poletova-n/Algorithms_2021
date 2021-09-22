c4c4b56418251a33e8caf97a9c3d78d48b8f8a3f
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:23:43 +0300

    2.5

--- lab2/2.1.cpp
+++ lab2/2.1.cpp
@@ -5,25 +5,32 @@ int main()
     int a, b, c, maxN;
     std::cout << "Enter three numbers" << std::endl;
     std::cin >> a >> b >> c;
-    if (a > b)
+    if (std::cin.peek() != '\n')
     {
-        maxN = a;
-        if (a < c)
-        {
-            maxN = c;
-        }
-    }
-    else if (a < b)
-    {
-        maxN = b;
-        if (b < c)
-        {
-            maxN = c;
-        }
+        std::cout << "Numbers must be entered. End of the program.";
     }
     else
     {
-        maxN = c;
+        if (a > b)
+        {
+            maxN = a;
+            if (a < c)
+            {
+                maxN = c;
+            }
+        }
+        else if (a < b)
+        {
+            maxN = b;
+            if (b < c)
+            {
+                maxN = c;
+            }
+        }
+        else
+        {
+            maxN = c;
+        }
+        std::cout << maxN;
     }
-    std::cout << maxN;
 }
