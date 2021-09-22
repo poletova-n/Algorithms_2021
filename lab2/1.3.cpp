ab60386c1049f09e696ce748a34ea42423128e2c
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:19:42 +0300

    2.5

--- lab2/1.3.cpp
+++ lab2/1.3.cpp
@@ -5,15 +5,28 @@ int main()
     int l1, l2, col1, col2;
     std::cout << "Enter line and column number of the bishop: " << std::endl;
     std::cin >> l1 >> col1;
-    std::cout << "Enter line and column number of another figure:" << std::endl;
-    std::cin >> l2 >> col2;
-    if (abs(l1-l2) == abs(col1-col2))
+    if (std::cin.peek() != '\n')
     {
-        std::cout << "The bishop takes a figure.";
+        std::cout << "The coordinates of the figures must be numbers. End of the program.";
     }
     else
     {
-        std::cout << "The bishop doesn't take a figure.";
+        std::cout << "Enter line and column number of another figure:" << std::endl;
+        std::cin >> l2 >> col2;
+        if (std::cin.peek() != '\n')
+        {
+            std::cout << "The coordinates of the figures must be numbers. End of the program.";
+        }
+        else
+        {
+            if (abs(l1-l2) == abs(col1-col2))
+            {
+                std::cout << "The bishop takes a figure.";
+            }
+            else
+            {
+                std::cout << "The bishop doesn't take a figure.";
+            }
+        }
     }
-
 }
