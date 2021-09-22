215cfbcaabe8806a709b072df9b517dd6c4be18f
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:18:26 +0300

    2.5

--- lab2/1.2.cpp
+++ lab2/1.2.cpp
@@ -5,14 +5,28 @@ int main()
     int l1, l2, col1, col2;
     std::cout << "Enter line and column number of the rook: " << std::endl;
     std::cin >> l1 >> col1;
-    std::cout << "Enter line and column number of another figure:" << std::endl;
-    std::cin >> l2 >> col2;
-    if ((l1==l2) or (col1 == col2))
+    if (std::cin.peek() != '\n')
     {
-        std::cout << "The rook takes a figure.";
+        std::cout << "The coordinates of the figures must be numbers. End of the program.";
     }
     else
     {
-        std::cout << "The rook doesn't take a figure.";
+        std::cout << "Enter line and column number of another figure:" << std::endl;
+        std::cin >> l2 >> col2;
+        if (std::cin.peek() != '\n')
+        {
+            std::cout << "The coordinates of the figures must be numbers. End of the program.";
+        }
+        else
+        {
+            if ((l1==l2) or (col1 == col2))
+            {
+                std::cout << "The rook takes a figure.";
+            }
+            else
+            {
+                std::cout << "The rook doesn't take a figure.";
+            }
+        }
     }
 }
