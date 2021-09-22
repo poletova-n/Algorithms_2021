098130e057c3a145ebabdfe355c0c21b6f43df6d
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-22 14:14:41 +0300

    2.5

--- lab2/1.1.cpp
+++ lab2/1.1.cpp
@@ -5,13 +5,20 @@ int main()
     int year;
     std::cout << "Enter year: " << std::endl;
     std::cin >> year;
-    if (((year % 4 == 0) or (year % 400 == 0)) and (year % 100 != 0))
+    if (std::cin.peek() != '\n')
     {
-        std::cout << "This is a leap year.";
+        std::cout << "The year must be a number. End of the program.";
     }
     else
     {
-        std::cout << "This isn't a leap year.";
+        if (((year % 4 == 0) or (year % 400 == 0)) and (year % 100 != 0))
+        {
+            std::cout << "This is a leap year.";
+        }
+        else
+        {
+            std::cout << "This isn't a leap year.";
+        }
     }
     return 0;
 }
