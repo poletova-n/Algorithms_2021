b21a3434ce9aa9bc780b317280f980d47c3140c5
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-23 14:29:56 +0300

    #4

--- lab4/c1.1.cpp
+++ lab4/c1.1.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
-const char* error_invalid_number = "Numbers must be entered. End of the program."
+const char* error_invalid_number = "Numbers must be entered. End of the program.";
 
-int main()
-{
-    int sum = 0, num = 0;
+int main() {
+    int sum = 0, num = -1;
     try
     {
-        std::cout << "Enter numbers: " <<std::endl;
+        std::cout << "Enter numbers: " << std::endl;
         while (num != 0)
         {
             std::cin >> num;
             if (std::cin.peek() != '\n')
             {
                 throw error_invalid_number;
-            }
-            else
-            {
+            } else {
                 sum += num;
             }
         }
-        catch (const char* error)
-        {
-            std::cerr << std::endl << error << std::endl;
-        }
+        std::cout << "Sum of all numbers: " << sum;
     }
-    std::cout << "Sum of all numbers: " << sum;
+    catch (const char *error)
+    {
+        std::cerr << std::endl << error << std::endl;
+    }
 }
