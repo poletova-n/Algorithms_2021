f3d989dbd82843aa76e0277ac5ff2dffc9eaf53d
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-24 13:45:24 +0300

    lab4

--- lab4/c2.5.cpp
+++ lab4/c2.5.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 
-const char* error_not_number = "The side of the triangle must be numbers. End of the program.";
-const char* error_less_zero = "The side of the square must be greatest than zero. End of the program.";
+const char* error_not_number = "The side of the rhombus must be numbers. End of the program.";
+const char* error_less_zero = "The side of the rhombus must be greatest than zero. End of the program.";
 
 int main()
 {
-    int N;
-    std::cout << "Enter the side of the triangle: " << std::endl;
+    int N, space;
+    std::cout << "Enter the side of the rhombus: " << std::endl;
     std::cin >> N;
     try
     {
+        space = N-1;
         if (std::cin.peek() != '\n')
         {
             throw error_not_number;
@@ -18,18 +19,41 @@ int main()
         {
             throw error_less_zero;
         }
-        for (int i = 1; i < N+1; i++)
+
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 1; j < i+1; j++)
+            for (int j = 0; j < space; j++)
             {
-                std::cout << "*";
+                std::cout << " ";
+            }
+            for (int j = 0; j < i+1; j++)
+            {
+                std::cout << "* ";
+            }
+
+            std::cout << std::endl;
+            space--;
+
+        }
+        space = 0;
+        for (int i = N; i > 0; i--)
+        {
+
+            for (int j = 0; j < space; j++)
+            {
+                std::cout << " ";
+            }
+            for (int j = 0; j < i; j++)
+            {
+                std::cout << "* ";
             }
             std::cout << std::endl;
+            space++;
+
         }
     }
-    catch (const char* error)
+    catch (const char *error)
     {
         std::cerr << std::endl << error << std::endl;
     }
-
-
+}
