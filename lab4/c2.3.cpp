14d9330fd22e53e168ac95634a7816bda7c6d717
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-24 13:03:49 +0300

    lab4

---
+++ lab4/c2.3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+const char* error_not_number = "The side of the square must be numbers. End of the program.";
+const char* error_less_zero = "The side of the square must be greatest than zero. End of the program.";
+
+int main()
+{
+    int N;
+    std::cout << "Enter the side of the square: " << std::endl;
+    std::cin >> N;
+    try
+    {
+        if (std::cin.peek() != '\n')
+        {
+            throw error_not_number;
+        }
+        if (N < 0)
+        {
+            throw error_less_zero;
+        }
+        for (int i = 1; i < N+1; i++)
+        {
+            for (int j = 1; j < N+1; j++)
+            {
+                std::cout << "*";
+            }
+            std::cout << std::endl;
+        }
+    }
+    catch (const char* error)
+    {
+        std::cerr << std::endl << error << std::endl;
+    }
+}
+
