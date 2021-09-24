19d17c40b02a1cd59e037714ebcd47ebd8c75e62
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-25 00:37:18 +0300

    ind1

--- ind1/ind1_2.cpp
+++ ind1/ind1_2.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+const char* error_not_number = "Only numbers must be entered. End of the program";
 
+int main()
+{
+    try
+    {
+        int num = INT_MIN, fMax = INT_MIN,sMax = INT_MIN;
+        std::cout << "Enter numbers: "<< std::endl;
+        while (num != 0)
+        {
+            std::cin >> num;
+            if (std::cin.peek() != '\n')
+            {
+                throw error_not_number;
+            }
+            if (num >= fMax)
+            {
+                sMax = fMax;
+                fMax = num;
+            }
+            else if(num > sMax)
+            {
+                sMax = num;
+            }
+        }
+        std::cout << "Second maximum number:" << sMax;
+    }
+    catch(const char* error)
+    {
+        std::cerr << std:: endl << error <<std::endl;
+    }
+}
