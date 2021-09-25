e66ece629ef202c42a6e9061cdbec721dc41c5db
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-09-25 14:50:06 +0300

    ind1

--- lab4/c2.5.cpp
+++ lab4/c2.5.cpp
@@ -39,11 +39,11 @@ int main()
         for (int i = N; i > 0; i--)
         {
 
-            for (int j = 0; j < space; j++)
+            for (int j = 0; j < space+1; j++)
             {
                 std::cout << " ";
             }
-            for (int j = 0; j < i; j++)
+            for (int j = 1; j < i; j++)
             {
                 std::cout << "* ";
             }
