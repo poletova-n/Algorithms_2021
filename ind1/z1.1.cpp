58f33c4b1403e8f6a071f21bce59a328b86fc58a
S-4I5 <yasha.poluektovv@gmail.com>
2021-09-26 19:14:45 +0300

    ind1 z1.1 fixes

--- ind1/z1.1.cpp
+++ ind1/z1.1.cpp
@@ -2,36 +2,32 @@
 
 const char* ERROR_INVALID_INPUT = "Error: input must only contain integers.";
 
-int input();
-
 int main()
 {
-        int first = 0;
-        int second = 0;
-        int third = 0;
-        int count = 0;
-        std::cout << "Enter numbers:" << std::endl;
-        first = input();
-        if(first == 0)
-        {
-            std::cout << "Number of strict local minima is " << count;
-            return 0;
-        }
-        second = input();
-        if(second == 0)
-        {
-            std::cout << "Number of strict local minima is " << count;
-            return 0;
-        }
+    int first = 0;
+    int second = 0;
+    int third = 0;
+    int count = 0;
+    std::cout << "Enter numbers:" << std::endl;
+    try
+    {
         do
         {
-            third = input();
-            if(third == 0)
+            std::cin >> third;
+            if (std::cin.fail())
             {
-                std::cout << "Number of strict local minima is " << count;
+                throw ERROR_INVALID_INPUT;
+            }
+            if (third == 0)
+            {
+                if (std::cin.peek() != 10)
+                {
+                    throw ERROR_INVALID_INPUT;
+                }
+                std::cout << "Number of strict local minima " << count;
                 break;
             }
-            if(first > second && second < third)
+            if (first > second && second < third)
             {
                 count++;
             }
@@ -39,23 +35,10 @@ int main()
             second = third;
         }
         while (third != 0);
-}
-
-int input()
-{
-    try
-    {
-        int input = 0;
-        std::cin >> input;
-        if(std::cin.fail() && std::cin.peek() != 10)
-        {
-            throw ERROR_INVALID_INPUT;
-        }
-        return input;
     }
-    catch(const char* error)
+    catch(const char *error)
     {
         std::cerr << std::endl << error << std::endl;
-        exit(-1);
+        return -1;
     }
 }
