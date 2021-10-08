c793c9a4b206e791d50fabb776f7aa9cacf62dd9
ce7er2s <spike.calhun@yandex.ru>
2021-10-08 23:16:22 +0300

    Индивидуальное задание №1

--- personal_labs/lab1/task1.11.cpp
+++ personal_labs/lab1/task1.11.cpp
@@ -3,5 +3,14 @@
 
 int main() {
 	int previous = 0;
+	int count = 0;
+	int temp = 0;
 	std::cin >> previous;
+	while (std::cin >> temp && temp != 0) {
+		if (temp % previous == 0) {
+			count++;
+		}
+		previous = temp;
+	}
+	std::cout << count;
 }
