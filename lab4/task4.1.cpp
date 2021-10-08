603a45bd8df2e0d472ca913bdc6d0531e50242bd
ce7er2s <spike.calhun@yandex.ru>
2021-10-08 13:47:36 +0300

    Сделана 4ая лаба

--- lab4/task4.1.cpp
+++ lab4/task4.1.cpp
@@ -7,13 +7,13 @@
 
 
 int main() {
-	uint32_t count = 0;
+	int count = 0;
 	int sum = 0;
 	uint32_t positive_count = 0;
 	uint32_t positive_sum = 0;
 	int maximal = 0;
 	uint32_t maximal_index = 0;
-	int minimal = 0;
+	uint32_t minimal = -1;
 	uint32_t minimal_index = 0;
 	int temp = 0; // Временное значение для входа в функцию
 	while ((std::cin >> temp) && (temp != 0)) { // Пока читается поток и пока временное значение не равно нулю.
@@ -27,12 +27,15 @@ int main() {
 			maximal_index = count;
 			maximal = temp;
 		}
-		if (minimal > temp) {
+		if (temp > 0 && minimal > temp) {
 			minimal_index = count;
 			minimal = temp;
 		}
 	}
-	std::cout << "Сумма всех введенных чисел: " << sum;
-	std::cout << "Cреднее арифметическое всех введенных чисел: " << static_cast<double>(sum/count);
-	std::cout << "Cреднее арифметическое всех положительных введенных чисел: " << static_cast<double>(sum/count);
+	std::cout << "Сумма всех введенных чисел: " << sum << std::endl;
+	std::cout << "Cреднее арифметическое всех введенных чисел: " << 1.0*sum/count) << std::endl;
+	std::cout << "Cреднее арифметическое всех положительных введенных чисел: " <<  1.0*positive_sum/positive_count << std::endl;
+	std::cout << "Максимальное значение и его номер среди всех введенных чисел: " << maximal << " " << maximal_index << std::endl;
+	std::cout << "Минимальное положительное значение и его номер среди всех введенных чисел: " << static_cast<int>(minimal) << " " << minimal_index << std::endl;
+	return 0;
 }
