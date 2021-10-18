e68a85ca65288d5e5f286aa13f00f82068398c7e
nohomoonly <regina.krivoshapkina@icloud.com>
2021-10-19 02:48:37 +0300

    sf

---
+++ lab4/lab4_2_2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	try
+	{
+		int a, b, c;
+		std::cout << "Введите координаты слона: ";
+		std::cin >> a >> b;
+		if (!std::cin)
+		{
+			throw "Ошибка: Вводить можно только числа";
+		}
+		if ((a <= 0 || a > 8) || (b <= 0 || b > 8))
+		{
+			throw "Ошибка: Вы вышли за границы доски";
+		}
+		std::cout << "Координаты, находящиеся под ударом слона: " << std::endl;
+		for (int i = 1; i <= 8; ++i)
+		{
+			for (int j = 1; j <= 8; ++j)
+			{
+				if ((abs(a - i) == abs(b - j)) && ((a != i) && (b != j)))
+				{
+				std::cout << "[" << i << " " << j << "]" << std::endl;
+				}
+			}
+		}
+	}
+	catch (const char* error)
+	{
+		std::cerr << std::endl << error << std::endl;
+		return -1;
+	}
+}
---
+++ lab4/lab4_2_4.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	try
+	{
+		int n;
+		std::cout << "Введите число: ";
+		std::cin >> n;
+		if (!std::cin)
+		{
+			throw "Ошибка: Вводить можно только числа";
+		}
+		if (n < 0)
+		{
+			throw "Ошибка: Вводить можно только положительные числа";
+		}
+		for (int i = 0; i < n; ++i)
+		{
+			std::cout << std::endl;
+			for (int j = 0; j < i + 1; ++j)
+			{
+				std::cout << "*";
+			}
+		}
+	}
+	catch (const char* error)
+	{
+		std::cerr << std::endl << error << std::endl;
+		return -1;
+	}
+}
