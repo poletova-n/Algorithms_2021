cd28159798468a7775df77d55dde1e66d2df1216
nohomoonly <regina.krivoshapkina@icloud.com>
2021-10-18 20:41:51 +0300

    hello

---
+++ lab3/lab3_1_3.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	int a, b; 
+	int a1, b1;
+	std::cout << "Введите координаты слона: ";
+	std::cin >> a >> b;
+	std::cout << "Введите координаты фигуры: ";
+	std::cin >> a1 >> b1;
+	try
+	{
+		if (!std::cin)
+		{
+			throw "Ошибка: должно быть задано число!";
+		}
+		if ((a <= 0 || a > 8) || (b <= 0 || b > 8) || (a1 <= 0 || a1 > 8) || (b1 <= 0 || b1 > 8))
+		{
+			throw "Вы вышли за границы";
+
+		}
+	}
+	catch (const char* error)
+	{
+		std::cerr << std::endl << error << std::endl;
+		return -1;
+	}
+	if (abs(a - a1) == abs(b - b1)) 
+	{
+		std::cout << "Да";
+	}
+	else
+	{
+		std::cout << "Неа";
+	}
+}
