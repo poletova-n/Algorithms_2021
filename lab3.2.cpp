7981bfcf43e199878883a3b8e427e0d1a495f235
olgaocrap <90508015+olgaocrap@users.noreply.github.com>
2021-10-19 22:47:25 +0300

    Add files via upload

---
+++ lab3.2.cpp
@@ -0,0 +1,40 @@
+﻿
+#include <iostream>
+using namespace std;
+const string ERROR_NOT_NUMBER = "error! enter the NUMBER";
+
+const string ERROR_NOT_POSITIVE_NUMBER = "error! enter positive number";
+int main() {
+	try {
+		int n(0);
+		cout << "vvedite kolichestvo voron: \n";
+		cin >> n;
+		if (cin.fail() || cin.peek() != 10) {
+			throw ERROR_NOT_NUMBER;
+		}
+		else if (n < 0) {
+			throw ERROR_NOT_POSITIVE_NUMBER;
+		}
+		else;
+	if (((n >= 11) and (n <= 19)) || (n % 10 == 0 || n % 10 == 5 || n % 10 == 6 || n % 10 == 7 || n % 10 == 8 || n % 10 == 9))
+		/*проверяем условия: N от 11 до 19, остаток от деления на 10 равен 0, 5, 6, 7, 8, 9*/
+	{
+		cout << "zdes " << n << " voron.";
+	}
+	else
+	{
+		if (n % 10 == 1) //проверяем условия: остаток от деления равен 1
+		{
+			cout << "zdes " << n << " vorona.";
+		}
+		else
+		{
+			cout << "zdes " << n << " vorony.";
+		}
+	}
+	}
+	catch (const string& error) {
+		cerr << endl << error << endl;
+		return -1;
+	}
+}
---
+++ lab3.3.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+const string ERROR_NOT_NUMBER = "error! enter the whole number.";
+
+const string ERROR_NOT_POSITIVE_NUMBER = "no such triangle exists. numbers must be positive.";
+
+const string ERROR_NOT_TRIANGLE = "no such triangle exists. c>a+b or c=a+b.";
+int main()
+{
+	try
+	{
+		cout << "enter the POSITIVE lengths of the sides of the triangle in ascending order \n";
+		double a, b, c;
+		cin >> a;
+		cin >> b;
+		cin >> c;
+		if (cin.fail() || cin.peek() != 10) {
+			throw ERROR_NOT_NUMBER;
+		}
+		if ((a < 0) || (a == 0) || (b < 0) || (b == 0) || (c < 0) || (c == 0))
+		{
+			throw ERROR_NOT_POSITIVE_NUMBER;
+		}
+		if (c >= (a + b))
+		{
+			throw ERROR_NOT_TRIANGLE;
+		}
+		else {
+			if (c * c == (a * a + b * b)) // проверяем теорему пифагора
+				cout << "rectangular triangle\n";
+			else
+				if ((a == b) and (b == c)) // проверяем равенство трех сторон
+				{
+					cout << "equilateral triangle\n";
+				}
+				else
+				{
+					if (c * c > (a * a + b * b))// проверяем на свойство остроугольного треуг
+					{
+						cout << "acute-angled triangle\n";
+					}
+					else
+					{
+						cout << "obtuse triangle\n";
+					}
+					if ((a == c) || (b == c) || (a == b)) // проверяем равенство двух сторон
+					{
+						cout << "isosceles triangle\n";
+					}
+				}
+		}
+	}
+	catch (const string& error) {
+		cerr << endl << error << endl;
+		return -1;
+	}
+}
