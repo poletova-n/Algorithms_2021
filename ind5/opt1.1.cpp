db667faf76de838f46bfaf381d6367af6f34ffd6
anushervonkhoni <anush@email.ru>
2021-11-03 16:02:21 +0300

    ind5

---
+++ ind5/opt1.1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+
+const char* ERROR_FILE_NOT_OPENED = "File didn't open!";
+
+const int size1 = 5;
+const int size2 = 4;
+
+void readFile(int (&input)[size1][size2], std::ifstream &fin, std::ofstream &fout);
+
+int main()
+{
+    try
+    {
+        int arr[size1][size2];
+
+        std::ifstream fin("C:\\Users\\anush\\Algorithms_2021\\ind5\\inputFile.txt");
+        std::ofstream fout("C:\\Users\\anush\\Algorithms_2021\\ind5\\outputFile.txt");
+        if (!fin.is_open() || !fout.is_open())
+        {
+            throw ERROR_FILE_NOT_OPENED;
+        }
+        else
+        {
+            readFile(arr, fin, fout);
+            fin.close();
+        }
+        return 0;
+    }
+    catch(const char * error)
+    {
+        std::cerr << std::endl << error << std::endl;
+        return -1;
+    }
+}
+
+void readFile(int (&input)[size1][size2], std::ifstream &fin, std::ofstream &fout)
+{
+    for(int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++) {
+            fin >> input[i][j];
+            if(input[i][j] != 0)
+            {
+                fout << input[i][j] << " ";
+            }
+        }
+        fout << std::endl;
+    }
+}
