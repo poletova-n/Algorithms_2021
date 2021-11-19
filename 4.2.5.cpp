ae97c76122a1716ec1281ef3886304648faeac26
olgaocrap <90508015+olgaocrap@users.noreply.github.com>
2021-11-19 10:43:52 +0300

    Update 4.2.5.cpp

--- 4.2.5.cpp
+++ 4.2.5.cpp
@@ -28,8 +28,8 @@ int main()
             probel--;
 
         }
-        probel = 0;
-        for (int i = N; i > 0; i--)
+        probel = 1;
+        for (int i = N-1; i > 0; i--)
         {
 
             for (int j = 0; j < probel; j++)
