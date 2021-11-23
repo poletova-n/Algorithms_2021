bec5df230c01b226e027bc76561000a43c51faa1
A192747 <petr.khuraskin@yandex.com>
2021-11-23 17:00:12 +0300

    Try 31

--- ind3/Task.cpp
+++ ind3/Task.cpp
@@ -1,136 +1,167 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include <limits>
 #include <stdlib.h>
-#include <time.h>
-#include <algorithm>
 
-const char* ERROR_INVALID_INPUT = "Either a non-integer positive number was entered, or a non-number was entered, end of the program!\a"; \
-const char* ERROR_INVALID_FILE_NUMBER = "An error occurred while reading from the file. Either the file was not found, or the sequence number not a positive integer!\a";
-
-int MyFunc(int array[], int count)
-{
-    int countThree = 1, countMax = 0;
-    for (int i = 1; i < count; i++)
-    {
-        if (array[i - 1] > array[i])
-        {
-            countThree++;
-        }
-        else
-        {
-            countMax = std::max(countMax, countThree);
-            countThree = 1;
-        }
-        countMax = std::max(countMax, countThree);
-    }
-    return countMax;
-}
+const char* ERROR_FILE_NOT_OPEN = "The file was not open!\n";
+const char* ERROR_INVALID_INPUT = "Either a non-integer  number was entered, or a non-number was entered, end of the program!\n";
+const int Columns = 5, Lines = 4;
+int CheckColumns(int Mass[][Columns], int const lines);
+int FindMinSum(int** Mass, int line, int columns);
+int** inputArray(std::ifstream& input, int** Mass, int line, int columns);
 
 int main()
 {
-    const int size = 5;
-    int arrayOne[size] = { 1, 6845, 654, 888, 789 };
-    int countOne = 1, countMaxOne = 0;
+	int Const_A[Lines][Columns] = { { 1, 2, 5, 4, 2 }, { 7, 8, 4, 5, 1 }, { 9, 9, 2, 6, 2 }, { 10, 3, 6, 7, 9 } };
+	std::cout << "Const Array: \n";
+	for (int i = 0; i < Lines; i++)
+	{
+		for (int j = 0; j < Columns; j++)
+		{
+			std::cout << Const_A[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "The number of columns whose elements are ordered in ascending order of elements is: " << CheckColumns(Const_A, Lines) << "\n";
 
-    countMaxOne = MyFunc(arrayOne, size);
-    std::cout << "Longest monotonically decreasing fragment length the sequence from the initialized array is " << countMaxOne << std::endl << std::endl;
+	std::ifstream input("C:/Users/PetrK/Documents/Visual Studio Files/ConsoleApplication16/input.txt");
+	std::ofstream output("C:/Users/PetrK/Documents/Visual Studio Files/ConsoleApplication16/output.txt");
+	int line = 0, columns = 0;
+	try
+	{
+		if (!input.is_open())
+		{
+			throw ERROR_FILE_NOT_OPEN;
+		}
 
-    int arrsize = 0;
-    try
-    {
-        std::cout << "Enter the number of elements in a dynamic array: ";
-        std::cin >> arrsize;
-        srand(time(0));
-        if ((!std::cin) or std::cin.fail() or (std::cin.peek() != 10))
-        {
-            throw ERROR_INVALID_INPUT;
-        }
-        int* arrayTwo = new int[arrsize];
+		input >> line >> columns;
 
-        std::cout << "Values in a dynamic array: " << std::endl;
+		if ((!input) or input.fail())
+		{
+			throw ERROR_INVALID_INPUT;
+		}
+	}
+	catch (const char* error)
+	{
+		std::cerr << "\n" << error;
+		return -1;
+	}
 
-        for (int i = 0; i < arrsize; i++)
-        {
-            arrayTwo[i] = rand() % 30001 - 15000;
-            std::cout << arrayTwo[i] << std::endl;
-        }
+	int** array = new int* [line];
+	for (int i = 0; i < line; i++)
+	{
+		array[i] = new int[columns];
+	}
 
-        int countTwo = 1, countMaxTwo = 0;
-        countMaxTwo = MyFunc(arrayTwo, arrsize);
+	output << "The minimum among the sums of the elements of the diagonals parallel to the main diagonal of the matrix: " << FindMinSum(inputArray(input, array, line, columns), line, columns);
+	input.close();
+	output.close();
+	return 0;
 
-        if (countMaxTwo != 1)
-        {
-            std::cout << "Longest monotonically decreasing fragment length a sequence from a dynamic array is " << countMaxTwo << std::endl << std::endl;;
-        }
-        else
-        {
-            std::cout << "There is no monotonically decreasing fragment in the sequence!" << std::endl;
-        }
-        delete[] arrayTwo;
-    }
-    catch (const char* error)
-    {
-        std::cerr << error;
-        return -1;
-    }
+}
 
-    int arrsizes = 0, countThree = 0, countMaxThree = 0, k = 0;;
+int CheckColumns(int Mass[][Columns], int const lines)
+{
+	int count = 0;
+	bool flag = 0;
+	for (int j = 0; j < Columns; j++)
+	{
+		flag = 0;
+		for (int i = 0; i < lines - 1; i++)
+		{
 
-    std::ifstream input("input.txt");
+			if (Mass[i][j] >= Mass[i + 1][j])
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0)
+		{
+			count++;
+		}
+	}
+	return count;
+}
 
-    try
-    {
-        while (!input.eof())
-        {
-            countThree = 1;
-            arrsizes = 0;
-            countMaxThree = 1;
-            input >> arrsizes;
-            k++;
-            if (arrsizes == 0)
-            {
-                return 0;
-            }
-            if ((!input) or input.fail() or (input.peek() != 10))
-            {
-                throw ERROR_INVALID_FILE_NUMBER;
-            }
 
-            int* arrayThree = new int[arrsizes];
+int** inputArray(std::ifstream& input, int** Mass, int line, int columns)
+{
+	for (int i = 0; i < line; i++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			input >> Mass[i][j];
+		}
+	}
+	return Mass;
+}
+int FindMinSum(int** Mass, int line, int columns)
+{
 
-            for (int i = 0; i < arrsizes; i++)
-            {
-                input >> arrayThree[i];
-                if ((!input) or input.fail() or (input.peek() != 10))
-                {
-                    delete[] arrayThree;
-                    throw ERROR_INVALID_FILE_NUMBER;
-                }
+	int k = 0, sum = 0, k1 = 1, SumMin1 = INT_MAX, SumMin2 = INT_MAX;
+	for (int k = 1; k < columns; k++)
+	{
+		for (int i = 0; i < line; i++)
+		{
+			for (int j = 0; j < columns; j++)
+			{
+				if ((i + k == j) and (i + k < columns))
+				{
+					if (k == k1)
+					{
+						sum = sum + Mass[i][j];
+					}
+					else
+					{
+						SumMin1 = std::min(sum, SumMin1);
+						sum = Mass[i][j];
+						k1 = k;
+					}
+					if (k == columns - 1)
+					{
+						SumMin1 = std::min(sum, SumMin1);
+						sum = Mass[i][j];
+						SumMin1 = std::min(sum, SumMin1);
+						k1 = k;
+					}
 
-            }
+				}
+			}
+		}
 
-            countMaxThree = MyFunc(arrayThree, arrsizes);
+	}
+	int h1 = 1;
+	sum = 0;
+	for (int h = 1; h < line; h++)
+	{
+		for (int j = 0; j < columns; j++)
+		{
+			for (int i = 0; i < line; i++)
+			{
+				if ((j + h == i) and (j + h < line))
+				{
+					if (h == h1)
+					{
+						sum = sum + Mass[i][j];
+					}
+					else
+					{
+						SumMin2 = std::min(sum, SumMin2);
+						sum = Mass[i][j];
+						h1 = h;
+					}
+					if (h == line - 1)
+					{
+						SumMin2 = std::min(sum, SumMin2);
+						sum = Mass[i][j];
+						SumMin2 = std::min(sum, SumMin2);
+						h1 = h;
+					}
 
-            std::cout << k << " array : ";
+				}
+			}
+		}
 
-            if (countMaxThree > 1)
-            {
-                std::cout << "The longest length of a monotonically decreasing fragment from a file is " << countMaxThree << std::endl;
-            }
-            else
-            {
-                std::cout << "There is no monotonically decreasing fragment in the sequence from file!" << std::endl;
-            }
-            delete[] arrayThree;
-        }
-        input.close();
-    }
-    catch (const char* error)
-    {
-        std::cerr << error;
-        return -1;
-    }
-    return 0;
+	}
+	return std::min(SumMin1, SumMin2);
 }
