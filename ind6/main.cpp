7389042fa9314d08d8f494d6c4b3eafab25bb708
ce7er2s <spike.calhun@yandex.ru>
2021-12-04 01:32:07 +0300

    ind6 string class (copy, init, destroy constructors + compare method). c-string style

--- ind6/main.cpp
+++ ind6/main.cpp
@@ -1,1 +1,8 @@
+#include "string.h"
 
+int main () {
+	String str1("1abcdef");
+	String str2("1abcdef");
+	int i = str1.compare(str2);
+	std::cout << i;
+}
---
+++ ind6/string.h
@@ -0,0 +1,79 @@
+#include <iostream>
+//
+// Created by reenie on 04.12.2021.
+//
+
+class String {
+private:
+	char* _raw_string;
+	size_t length;
+public:
+	String () {
+		*(this->_raw_string) = '\0';
+	}
+
+	String (char* str) {
+		size_t i = 0;
+		while (str[i] != '\0') { // поиск длины строки
+			i++;
+		}
+
+		this->_raw_string = new char[i]; // выделение памяти на строку
+		for (size_t j = 0; j < i; j++) {
+			this->_raw_string[j] = str[j]; // поэлементное копирование
+		}
+		this->_raw_string[i] = '\0'; // терминальный символ в конце
+
+		this->length = i-1; // запись длины
+	}
+
+	String (const String& str) {
+		this->_raw_string = new char[str.length];
+		for (size_t i = 0; i < str.length; i++) {
+			this->_raw_string[i] = str._raw_string[i];
+		}
+		this->_raw_string[str.length] = '\0';
+		this->length = str.length;
+	}
+
+	~String () {
+		delete[] this->_raw_string;
+	}
+
+	size_t get_length () {
+		return this->length;
+	}
+
+	char& operator[] (size_t index) {
+		if (index < this->length) {
+			return this->_raw_string[index];
+		} else {
+			throw std::exception();
+		}
+	}
+
+	int32_t compare (String& str) {
+		size_t i = 0;
+		while ((i < this->length) && (i < str.length)) {
+			if ((*this)[i] < str[i]) {
+				return -1;
+			} else if ((*this)[i] > str[i]) {
+				return  1;
+			} else {
+				i++;
+			}
+		}
+		if (this->length < str.length) {
+			return -1;
+		} else if (this->length > str.length) {
+			return  1;
+		} else {
+			return 0;
+		}
+	}
+};
+
+#ifndef ALGORITHMS_2021_STRING_H
+#define ALGORITHMS_2021_STRING_H
+
+#endif //ALGORITHMS_2021_STRING_H
