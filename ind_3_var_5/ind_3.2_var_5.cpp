c4f69d82d071bdaf01bbac31d24e4d6057fc77ae
olgaocrap <90508015+olgaocrap@users.noreply.github.com>
2021-12-04 01:42:39 +0300

    Add files via upload

---
+++ ind_3_var_5/ind_3.2_var_5.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+const char* error= "enter correct number";
+int main()
+{
+	srand((unsigned)time(NULL));
+	int n=0;
+	cout << "enter the number" << endl;
+	cin >> n;
+	if (cin.fail() || cin.peek() != '\n' || n < 1)
+	{
+		cerr << error << endl;
+		return -1;
+	}
+	int* array = new int[n];
+	int max = numeric_limits<int>::min();
+	
+
+	for (int i = 0; i < n; i++)
+	{
+		array[i] = rand();
+		if (array[i] % 2 == 0)
+		{
+			cout << array[i] << endl;
+		}
+
+	}
+	for (int j = 0; j < n; j++)
+	{
+
+		if (array[j] % 2 == 1)
+		{
+			cout << array[j] << endl;
+		}
+
+	}
+}
+
