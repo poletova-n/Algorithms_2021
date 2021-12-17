14c84e6a860ca2eab4e2c82d8116291e957a3689
MemberOFsct <elizaveta.vazhenina@mail.ru>
2021-12-17 19:07:35 +0300

    ind7

--- ind7/1.2.cpp
+++ ind7/1.2.cpp
@@ -2,18 +2,22 @@
 #include <string>
 
 bool isSign(std::string& num);
-bool afterSign(std::string& num);
-bool isRealNum(std::string& num);
 bool WhereIsE(std::string& num);
+bool realNum(std::string& num);
+bool beforeSign(std::string& num);
+bool afterSign(std::string& num);
 
 int main()
 {
     std::string num;
     std::cin >> num;
-    if ((num[0] == '+') or (num[0] == '-') or (num[0] <= '9') and (num[0] >= '0') )
+    if ((num[1] >= '0') and (num[0] <= '9') and (num[1] == 'E'))
     {
+        std::cout << false;
+    }
+    else if ((((num[0] == '+') or (num[0] == '-')) or ((num[0] >= '0') and (num[0] <= '9')))) {
         num.erase(num.begin());
-        std::cout << isRealNum(num);
+        std::cout << realNum(num);
     }
     else
     {
@@ -23,7 +27,47 @@ int main()
 
 bool isSign(std::string& num)
 {
-    if (num[0] == '+' or num[0] == '-')
+    if ((num[0] == '+') or (num[0] == '-'))
+    {
+        num.erase(num.begin());
+        return afterSign(num);
+    }
+    else
+    {
+        return afterSign(num);
+    }
+}
+
+bool realNum(std::string& num)
+{
+    int iter = 0;
+    int dot = 0;
+    if ((num[iter] >= '0') and (num[iter] <= '9') )
+    {
+        iter++;
+        num.erase(num.begin());
+        return realNum(num);
+    }
+    if (num[iter] == '.')
+    {
+        dot++;
+    }
+    if ((num[iter] == '.') and (num[iter]!= 'E'))
+    {
+        num.erase(num.begin());
+        return WhereIsE(num);
+    }
+    if (dot != 1)
+    {
+        return false;
+    }
+    else if ((num[iter] == 'E') and (num[iter+1] == '+' or num[iter+1] == '-'))
+    {
+        num.erase(num.begin());
+        num.erase(num.begin());
+        return afterSign(num);
+    }
+    else if (num[iter] == 'E')
     {
         num.erase(num.begin());
         return afterSign(num);
@@ -33,41 +77,17 @@ bool isSign(std::string& num)
         return false;
     }
 }
-bool afterSign(std::string& num)
-{
-    int iter = 0;
-    if (num[iter] <= '9' and num[iter] >= '0')
-    {
-        iter++;
-        if (num.size() == 1)
-        {
-            return true;
-        }
-        else
-        {
-            num.erase(num.begin());
-            return afterSign(num);
-        }
-    }
-    else
-    {
-        return false;
-    }
-}
+
 bool WhereIsE(std::string& num)
 {
     int iter = 0;
-    if (num[iter] <= '9' and num[iter] >= '0')
+    if ((num[iter] >= '0') and (num[iter] <= '9'))
     {
         iter++;
         num.erase(num.begin());
         return WhereIsE(num);
     }
-    else if (num[iter] == 'E' and num[iter+1] <= '9' and num[iter+1] >= '0' )
-    {
-        return true;
-    }
-    else if (num[iter] == 'E' and num[iter+1])
+    else if (num[iter]== 'E')
     {
         num.erase(num.begin());
         return isSign(num);
@@ -77,29 +97,44 @@ bool WhereIsE(std::string& num)
         return false;
     }
 }
-bool isRealNum(std::string& num)
+
+bool afterSign(std::string& num)
 {
     int iter = 0;
-    if (num[iter] <= '9' and num[iter] >= '0')
-    {
+    if ((num[iter] >= '0') and (num[iter]<= '9')) {
+        if (num.length() == 1)
+        {
+            return true;
+        }
         iter++;
         num.erase(num.begin());
-        return isRealNum(num);
-    }
-    else if(num[iter] == '.' and num[iter+1] != 'E')
-    {
-        num.erase(num.begin());
-        return WhereIsE(num);
-    }
-    else if (num[iter] == 'E')
-    {
-        num.erase(num.begin());
-        return isSign(num);
-
+        return afterSign(num);
     }
     else
     {
         return false;
     }
 }
+bool beforeSign(std::string& num)
+{
+    int iter = 0;
+    if ((num[iter] >= '0') and (num[iter]<= '9'))
+    {
+        if (num.length() == 1)
+        {
+            return true;
+        }
+        iter++;
+        num.erase(num.begin());
+        return afterSign(num);
 
+    }
+    else if (num[iter] == '.')
+    {
+        return afterSign(num);
+    }
+    else
+    {
+        return false;
+    }
+}
