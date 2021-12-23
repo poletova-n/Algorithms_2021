a6becee6b748c21d9719c2d57f7d68f9ee654337
olgaocrap <90508015+olgaocrap@users.noreply.github.com>
2021-12-23 17:59:42 +0300

    Update ind_4_var_5.cpp

--- ind_4_var_5/ind_4_var_5.cpp
+++ ind_4_var_5/ind_4_var_5.cpp
@@ -1,52 +1,58 @@
-﻿#include <iostream>
+
+#include <iostream>
 #include <fstream>
 #include <windows.h>
 
+
 using namespace std;
 
-const char* ERROR_FILE_IS_NOT_OPENED = "file is not opened!";
+const char* ERROR_FILE_NOT_OPENED = "file is not open";
 
-char* findsym(const char* arr1, int size1, const char* arr2, int size2);
+char* findSym(const char* arr1, int size1, char* arr2, int size2);
 
 int main() 
 {
-
-    try 
+    SetConsoleOutputCP(CP_UTF8);
+    try
     {
         bool flag = false;
-        int position(0), size(0), size1(0), size2(0);
+        int position(0);
+        int size(0);
         char* arr1 = new char[size];
         char* arr2 = new char[size];
+        int size1(0);
+        int size2(0);
         ifstream fin("C:\\Users\\Ольга\\Documents\\учеба\\поликек\\c++\\Algorithms_2021\\ind_4_var_5\\input.txt");
-        ofstream fout("C:\\Users\\Ольга\\Documents\\учеба\\поликек\\c++\\Algorithms_2021\\ind_4_var_5\\output.txt");
+       
         if (!fin.is_open()) 
-        
         {
-            throw ERROR_FILE_IS_NOT_OPENED;
+            throw ERROR_FILE_NOT_OPENED;
         }
         else 
         {
             while (!fin.eof()) 
             {
+
                 fin.seekg(position);
                 fin >> size;
 
                 char* arr = new char[size];
 
                 fin.seekg(position + 3);
-                char cursym;
+                char currentSymbol;
                 int i(0);
-
-                while (fin.get(cursym) && i < size) 
+                while (fin.get(currentSymbol) && i < size)
                 {
-                    arr[i] = cursym;
+                    
+                    arr[i] = currentSymbol;
                     i++;
+                   
                 }
-
                 arr[size] = '\0';
 
                 position += size + 5;
-                if (flag == false) 
+               
+                if (flag == false)
                 {
                     arr1 = arr;
                     size1 = size;
@@ -58,10 +64,10 @@ int main()
                 }
                 flag = true;
             }
-            findsym(arr1, size1, arr2, size2);
+            findSym(arr1, size1, arr2, size2);
         }
         fin.close();
-        fout.close();
+      
         return 0;
     }
     catch (const char* error)
@@ -71,16 +77,21 @@ int main()
     }
 }
 
-char* findsym(const char* arr1, int size1, const char* arr2, int size2)
+char* findSym(const char* arr1, int size1, char* arr2, int size2) 
 {
+    fstream fout("C:\\Users\\Ольга\\Documents\\учеба\\поликек\\c++\\Algorithms_2021\\ind_4_var_5\\output.txt");
+   
     for (int i = 0; i < size1; ++i) 
     {
-        for (int j = 0; j < size2; ++j) 
+        for (int j = 0; j < size2; ++j)
         {
             if (arr1[i] != arr2[j]) 
             {
-                сout << arr2[j];
+                fout << arr2[j];
+                fout << ", ";
             }
         }
     }
+    fout.close();
+    return arr2;
 }
